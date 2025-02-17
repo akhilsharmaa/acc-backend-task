1b2832458129815ca40a949d353632da43565084
Akhilesh <akhilsharma3332@gmail.com>
2025-02-17 12:48:53 +0530

    [intial commit] ts + hello world server created.

---
+++ app.ts
@@ -0,0 +1,23 @@
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv'; 
+import listEndpoints from 'express-list-endpoints';
+import bodyParser from 'body-parser'
+
+const app = express();
+dotenv.config()
+
+app.use(bodyParser.json({limit: "100mb"}))
+app.use(bodyParser.urlencoded({limit:"50mb", extended: true}))
+
+const PORT = process.env.PORT || 3000; 
+
+app.get('/', (req, res) => {
+    res.send({
+        "message": "welcode to accredian assignment"
+    })
+})
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`[🎄] Server is running on http://localhost:${PORT}`);
+});
