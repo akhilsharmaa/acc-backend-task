81bf31682c773c20f3d0c246df6b08396d62bb04
Akhilesh <akhilsharma3332@gmail.com>
2025-02-18 18:23:17 +0530

    [cores] added.

--- app.ts
+++ app.ts
@@ -1,23 +1,30 @@
-import express, {Express, Request, Response} from 'express';
-import dotenv from 'dotenv';  
-import bodyParser from 'body-parser'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';  // Import CORS
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
-app.use(bodyParser.json({limit: "100mb"}))
-app.use(bodyParser.urlencoded({limit:"50mb", extended: true}))
+app.use(cors({
+    origin: '*',  
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 
-const PORT = process.env.PORT || 4000; 
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send({
-        "message": "welcome to accredian assignment backend"
-    })
-})
+        "message": "Welcome to Accredian Assignment Backend"
+    });
+});
 
 import referNew from './routes/refer/new';
-app.use('/refer', [referNew]);
+app.use('/refer', referNew);  // No need for array notation []
 
 // Start the server
 app.listen(PORT, () => {
